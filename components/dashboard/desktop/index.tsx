31ac6db506634c57a90e9520171420976822aaa9
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-06-30 15:47:37 +0200

    feat: add buttons to dashboard components

--- components/dashboard/desktop/index.tsx
+++ components/dashboard/desktop/index.tsx
@@ -4,7 +4,21 @@ import styles from "./main.module.scss";
 export default function DesktopDashboard() {
     return (
         <div className={styles.mainContainer}>
-            <h1 className={styles.test}>Desktop Dashboard</h1>
+            <div className={styles.firstRow}>
+                <div className={styles.schedule}>Schedule</div>
+            </div>
+            <div className={styles.secondRow}>
+                <div className={styles.employee}>Employee</div>
+                <div className={styles.notification}>Notification</div>
+            </div>
+            <div className={styles.thirdRow}>
+                <div className={styles.chat}>Chat</div>
+                <div className={styles.vacations}>Vacation</div>
+            </div>
+            <div className={styles.fourthRow}>
+                <div className={styles.statistic}>Statistic</div>
+                <div className={styles.plannerAi}>Panner AI</div>
+            </div>
         </div>
     )
 }
