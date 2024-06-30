574e0b39b559febf44961b9fb126fdf3c5de0cc9
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-06-30 12:20:14 +0200

    refactor: reorganize file structure

---
+++ components/dashboard/desktop/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./main.module.scss";
+
+export default function DesktopDashboard() {
+    return (
+        <div className={styles.mainContainer}>
+            <h1 className={styles.test}>Desktop Dashboard</h1>
+        </div>
+    )
+}
