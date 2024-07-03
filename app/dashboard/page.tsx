04c909ae54bdbdd39c31df8898a7fcf092efbf29
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-07-03 10:54:14 +0200

    feat: dashboard page completed

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarTop from "@/components/navbar/top";
+import DashboardCenter from "@/components/dashboard";
+
+export default function Dashboard() {
+
+    return (
+        <div>
+            <NavbarTop />
+            <DashboardCenter />
+        </div>
+    )
+}
