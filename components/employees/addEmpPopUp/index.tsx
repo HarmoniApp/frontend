77d9312c1ca177a3b20e39ee9f444d078ef407e3
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-08-14 10:52:33 +0200

    feat: created supervisor and department select

--- components/employees/addEmpPopUp/index.tsx
+++ components/employees/addEmpPopUp/index.tsx
@@ -12,10 +12,30 @@ interface Contract {
   id: number;
   name: string;
 }
-
+interface Language {
+  id: number;
+  name: string;
+}
+interface Supervisor {
+  id: number;
+  firstname: string;
+  surname: string;
+  role: {
+    id: number;
+    name: string;
+  }[];
+  employeeId: string;
+}
+interface Department {
+  id: number;
+  departmentName: string;
+}
 const AddEmpPopUp = () => {
   const [roles, setRoles] = useState<Role[]>([]);
   const [contracts, setContracts] = useState<Contract[]>([]);
+  const [languages, setLanguages] = useState<Language[]>([]);
+  const [supervisors, setSupervisors] = useState<Supervisor[]>([]);
+  const [departments, setDepartments] = useState<Department[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:8080/api/v1/role')
@@ -30,6 +50,29 @@ const AddEmpPopUp = () => {
       .then(data => setContracts(data))
       .catch(error => console.error('Error fetching contract-type:', error));
   }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/language')
+      .then(response => response.json())
+      .then(data => setLanguages(data))
+      .catch(error => console.error('Error fetching languages:', error));
+  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/user/supervisor')
+      .then(response => response.json())
+      .then(data => setSupervisors(data))
+      .catch(error => console.error('Error fetching supervisors:', error));
+  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/address/departments')
+      .then(response => response.json())
+      .then(data => setDepartments(data))
+      .catch(error => console.error('Error fetching supervisors:', error));
+  }, []);
+
+
 
   return (
     <div>
@@ -79,20 +122,44 @@ const AddEmpPopUp = () => {
         <div className={styles.contractContainerMain}>
           <p>UMOWA:</p>
           <select name="contract" id="" required>
-            <option selected disabled>Wybierz umowe:</option>
+            <option value="" disabled>Wybierz umowe:</option>
             {contracts.map(contract => (
-              <option key={contract.id}>
+              <option key={contract.id} value={contract.id}>
                 {contract.name}
               </option>
             ))}
           </select>
           <label>
-          DATA ZAWARCIA: <input type='date' name="startDate" required />
-        </label>
-        <label>
-          DATA ZAKOŃCZENIA: <input type='date' name="endDate" required />
-        </label>
+            DATA ZAWARCIA: <input type='date' name="startDate" required />
+          </label>
+          <label>
+            DATA ZAKOŃCZENIA: <input type='date' name="endDate" required />
+          </label>
         </div>
+        <div className={styles.supervisorContainerMain}>
+          <p>PRZEŁOŻONY:</p>
+          <select name="supervisor" id="" required>
+            <option value="" disabled>Wybierz przełożonego:</option>
+            {supervisors.map(supervisor => (
+              <option key={supervisor.id} value={supervisor.employeeId}>
+                {supervisor.firstname} {supervisor.surname} → {supervisor.role.map(role => role.name).join(', ')}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.departmentContainerMain}>
+          <p>ODDZIAŁ:</p>
+          <select name="department" id="" required>
+            <option value="" disabled>Wybierz oddział:</option>
+            {departments.map(department => (
+              <option key={department.id} value={department.id}>
+                {department.departmentName}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className={styles.fifthCol}>
         <div className={styles.roleContainerMain}>
           <p>ROLA:</p>
           {roles.map(role => (
@@ -102,6 +169,18 @@ const AddEmpPopUp = () => {
                 value={role.id}
               />
               {role.name}
+            </label>
+          ))}
+        </div>
+        <div className={styles.languagesContainerMain}>
+          <p>Języki:</p>
+          {languages.map(language => (
+            <label key={language.id} >
+              <input
+                type="checkbox"
+                value={language.id}
+              />
+              {language.name}
             </label>
           ))}
         </div>
