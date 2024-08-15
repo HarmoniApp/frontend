32297ff5b92e01195b733670e2dd367b006b18a9
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-08-15 14:43:15 +0200

    feat: prepare file for two views in vacations

---
+++ components/vacations/employees/index.tsx
@@ -0,0 +1,10 @@
+import styles from './main.module.scss';
+
+const VacationsEmployees = () => {
+    return (
+        <div>
+            <h1>EmployeesVacations</h1>
+        </div>
+    )
+}
+export default VacationsEmployees
---
+++ components/vacations/employer/index.tsx
@@ -0,0 +1,10 @@
+import styles from './main.module.scss';
+
+const VacationsEmployer = () => {
+    return (
+        <div>
+            <h1>EmployerVacations</h1>
+        </div>
+    )
+}
+export default VacationsEmployer
