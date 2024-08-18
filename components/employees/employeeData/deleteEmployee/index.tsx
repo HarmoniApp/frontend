519de9c41669e48af3f02409c3ee07a2804c4436
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-08-18 12:31:55 +0200

    fix: removing overlapping modalities and creating a dynamic one.

--- components/employees/employeeData/deleteEmployee/index.tsx
+++ components/employees/employeeData/deleteEmployee/index.tsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Modal from 'react-modal';
-import styles from './main.module.scss';
 import DeleteEmployeeConfirmationPopUp from '@/components/employees/employeeData/deleteEmployee/deleteEmployeeConfirmation';
-
-Modal.setAppElement('#root');
+import styles from './main.module.scss';
 
 interface DeleteEmployeeProps {
   userId: number;
@@ -18,10 +15,7 @@ interface DeleteEmployeeProps {
 }
 
 const DeletaEmployee: React.FC<DeleteEmployeeProps> = ({ userId, firstName, surname, roles, departmentName, onClose }) => {
-  const [modalIsOpenDeleteEmployeeConfirmation, setModalDeleteEmployeeConfirmation] = useState(false);
-  const openModaDeleteEmployeeConfirmation = () => setModalDeleteEmployeeConfirmation(true);
-  const closeModalDeleteEmployee = () => setModalDeleteEmployeeConfirmation(false);
-
+  const [modalStage, setModalStage] = useState<'confirm' | 'delete'>('confirm');
   const [modalCountdown, setModalCountdown] = useState(10);
   const [employeeLink, setEmployeeLink] = useState<string | null>(null);
   const router = useRouter();
@@ -31,13 +25,11 @@ const DeletaEmployee: React.FC<DeleteEmployeeProps> = ({ userId, firstName, surn
       method: 'DELETE',
     })
       .then(() => {
-        openModaDeleteEmployeeConfirmation();
-        setModalDeleteEmployeeConfirmation(true);
+        setModalStage('delete');
         const countdownInterval = setInterval(() => {
           setModalCountdown(prev => {
             if (prev === 1) {
               clearInterval(countdownInterval);
-              setModalDeleteEmployeeConfirmation(false);
               router.push("/employees");
               onClose();
             }
@@ -48,28 +40,25 @@ const DeletaEmployee: React.FC<DeleteEmployeeProps> = ({ userId, firstName, surn
       })
       .catch(error => console.error('Error deleting employee:', error));
   };
+
   return (
     <div>
-      <p>Czy na pewno chcesz usunąć użytkownika Imię:{firstName}, Nazwisko:{surname}, role: {roles.map(role => role.name).join(', ')}, w oddziale {departmentName}?</p>
-      <button onClick={handleDeleteEmployee}>Usuń</button>
-      <button onClick={onClose}>Cofnij</button>
-
-      <Modal
-        isOpen={modalIsOpenDeleteEmployeeConfirmation}
-        contentLabel="Delete Employee"
-        className={styles.modalContent}
-        overlayClassName={styles.modalOverlay}
-      >
-        <DeleteEmployeeConfirmationPopUp
-          firstName={firstName}
-          surname={surname}
-          employeeLink={employeeLink}
-          onClose={closeModalDeleteEmployee}
-          modalCountdown={modalCountdown}
-        />
-      </Modal>
+      {modalStage === 'confirm' ? (
+          <div>
+            <p>Czy na pewno chcesz usunąć użytkownika Imię: {firstName}, Nazwisko: {surname}, role: {roles.map(role => role.name).join(', ')}, w oddziale {departmentName}?</p>
+            <button onClick={handleDeleteEmployee}>Usuń</button>
+            <button onClick={onClose}>Cofnij</button>
+          </div>
+        ) : (
+          <DeleteEmployeeConfirmationPopUp
+            firstName={firstName}
+            surname={surname}
+            employeeLink={employeeLink}
+            onClose={onClose}
+            modalCountdown={modalCountdown}
+          />
+        )}
     </div>
   );
-
 };
 export default DeletaEmployee;
