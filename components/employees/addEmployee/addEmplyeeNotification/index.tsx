16a72f71ba5fe6a03cd661ac51798dfbcf598433
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-08-21 15:44:35 +0200

    refactor: class names to make them clearer in addEmployeeNotification.

--- components/employees/addEmployee/addEmplyeeNotification/index.tsx
+++ components/employees/addEmployee/addEmplyeeNotification/index.tsx
@@ -11,20 +11,20 @@ interface AddEmployeeConfirmationProps {
 const AddEmployeeNotification: React.FC<AddEmployeeConfirmationProps> = ({ firstname, surname, employeeLink, modalCountdown, onClose }) => {
     return (
         <div className={styles.addEmplyeeConfirmationContainerMain}>
-            <div className={styles.questionContainer}>
-                <p className={styles.questionParagraph}>Właśnie dodałeś pracownika:</p>
+            <div className={styles.headerContainer}>
+                <p className={styles.headerParagraph}>Właśnie dodałeś pracownika:</p>
                 <span className={styles.highlight}>{firstname} {surname}</span>
             </div>
             <div className={styles.counterContainter}>
                 <p className={styles.counterParagraph}>Powrót do listy pracowników za:</p>
-                <div className={styles.counterTimer}>
+                <div className={styles.counterTimerContainer}>
                     <span className={styles.highlight}>{modalCountdown}</span>
-                    <p className={styles.counterParagraph}>sekund.</p>
+                    <p className={styles.counterTimerParagraph}>sekund.</p>
                  </div>
             </div>
             <div className={styles.buttonConianer}>
                 {employeeLink && <button className={styles.employeeInfo}><a className={styles.a} href={employeeLink}>Szczegóły</a></button>}
-                <button className={styles.backButton} onClick={onClose}>Zamknij</button>
+                <button className={styles.closeButton} onClick={onClose}>Zamknij</button>
             </div>
         </div>
     );
