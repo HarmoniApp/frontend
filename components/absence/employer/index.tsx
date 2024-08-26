f19850aa1c22a8992af96efa7d73540d4f53cc8b
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-08-26 16:22:48 +0200

    feat: implementation of sorting.

--- components/absence/employer/index.tsx
+++ components/absence/employer/index.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import AbsenceCard from '@/components/absence/employer/absentCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRectangleList, faGrip } from '@fortawesome/free-solid-svg-icons';
+import AbsenceCard from '@/components/absence/employer/absentCard';
 import Absence from '@/components/types/absence';
 import AbsenceStatus from '@/components/types/absenceStatus';
 import styles from './main.module.scss';
-
-
 
 const AbsenceEmployer: React.FC = () => {
   const [absences, setAbsences] = useState<Absence[]>([]);
   const [absencesStatus, setAbsencesStatus] = useState<AbsenceStatus[]>([]);
   const [viewMode, setViewMode] = useState('tiles');
+  const [selectedStatus, setSelectedStatus] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     fetch('http://localhost:8080/api/v1/absence')
@@ -19,33 +18,66 @@ const AbsenceEmployer: React.FC = () => {
       .then(data => setAbsences(data))
       .catch(error => console.error('Error fetching absences:', error));
 
-      fetch('http://localhost:8080/api/v1/status')
+    fetch('http://localhost:8080/api/v1/status')
       .then(response => response.json())
       .then(data => setAbsencesStatus(data))
-      .catch(error => console.error('Error fetching absences:', error));
+      .catch(error => console.error('Error fetching absence statuses:', error));
   }, []);
+
+  const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const status = event.target.value;
+
+    if (status === 'clear') {
+      setSelectedStatus(undefined);
+      fetch('http://localhost:8080/api/v1/absence')
+        .then(response => response.json())
+        .then(data => setAbsences(data))
+        .catch(error => console.error('Error fetching absences:', error));
+    } else {
+      setSelectedStatus(status);
+      fetch(`http://localhost:8080/api/v1/absence/status/${status}`)
+        .then(response => response.json())
+        .then(data => setAbsences(data))
+        .catch(error => console.error('Error fetching absences by status:', error));
+    }
+  };
 
   return (
     <div className={styles.absenceEmployerContainerMain}>
       <div className={styles.absenceEmployerContainer}>
         <div className={styles.buttonContainer}>
           <div className={styles.selectContainer}>
-            <select className={styles.roleSelect} name="status" id="">
-              <option className="defalutOption" selected disabled>Filtruj</option>
+            <select
+              className={styles.roleSelect}
+              name="status"
+              onChange={handleStatusChange}
+              value={selectedStatus || 'default'}
+            >
+              <option value="default" disabled>Filtruj</option>
               {absencesStatus.map(absenceStatus => (
-                <option className={styles.filterOption} key={absenceStatus.id} value={absenceStatus.name}>{absenceStatus.name}</option>
+                <option
+                  className={styles.filterOption}
+                  key={absenceStatus.id}
+                  value={absenceStatus.name}
+                >
+                  {absenceStatus.name}
+                </option>
               ))}
-              <option className="clearFilter" value="clear">Wyczyść filtry</option>
+              <option className={styles.clearFilter} value="clear">Wyczyść filtry</option>
             </select>
           </div>
           <div className={styles.viewContainer}>
             <button
               className={`${styles.listViewButton} ${viewMode === 'list' ? styles.active : ''}`}
-              onClick={() => setViewMode('list')}> <FontAwesomeIcon icon={faRectangleList} />
+              onClick={() => setViewMode('list')}
+            >
+              <FontAwesomeIcon icon={faRectangleList} />
             </button>
             <button
               className={`${styles.tilesViewButton} ${viewMode === 'tiles' ? styles.active : ''}`}
-              onClick={() => setViewMode('tiles')}><FontAwesomeIcon icon={faGrip} />
+              onClick={() => setViewMode('tiles')}
+            >
+              <FontAwesomeIcon icon={faGrip} />
             </button>
           </div>
         </div>
@@ -62,5 +94,4 @@ const AbsenceEmployer: React.FC = () => {
     </div>
   );
 };
-
 export default AbsenceEmployer;
