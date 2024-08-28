fb3c3892c487b938356182c9a14b2be11d48cb91
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-08-28 18:54:28 +0200

    feat: add roles name under shifts.

--- components/schedule/calendar/calendarRow/shiftItem/index.tsx
+++ components/schedule/calendar/calendarRow/shiftItem/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Role from '@/components/types/role';
 import styles from './main.module.scss';
 
 interface Shift {
@@ -20,14 +21,32 @@ const ShiftItem: React.FC<ShiftItemProps> = ({ shifts }) => {
     return date.toLocaleTimeString('pl-PL', { hour: '2-digit', minute: '2-digit' });
   };
 
+  const roleIds = shifts.map((shift) => shift.role_id);
+  const [roles, setRoles] = useState<Role[]>([]);
+
+  useEffect(() => {
+    if (roleIds.length > 0) {
+      const roleIdsString = roleIds.join(',');
+      
+      fetch(`http://localhost:8080/api/v1/role?ids=${roleIdsString}`)
+        .then((response) => response.json())
+        .then((data) => setRoles(data))
+        .catch((error) => console.error('Error fetching roles:', error));
+    }
+  }, [roleIds]);
+
   return (
     <div className={styles.shiftItem}>
       {shifts.length > 0 ? (
-        shifts.map((shift) => (
-          <div key={shift.id} className={styles.shiftDetails}>
-            {formatTime(shift.start)} - {formatTime(shift.end)}
-          </div>
-        ))
+        shifts.map((shift) => {
+          const roleName = roles.find(role => role.id === shift.role_id)?.name || 'Brak roli';
+          return (
+            <div key={shift.id} className={styles.shiftDetails}>
+              {formatTime(shift.start)} - {formatTime(shift.end)}
+              <div>{roleName}</div>
+            </div>
+          );
+        })
       ) : (
         <div className={styles.noShift}>Brak zmiany</div>
       )}
