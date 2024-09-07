048234126dcff280c1587c3c80fa572efafa3eb6
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-09-07 10:49:39 +0200

    feat: adding a distinct style for today.

--- components/schedule/employer/calendar/calendarHeader/index.tsx
+++ components/schedule/employer/calendar/calendarHeader/index.tsx
@@ -1,27 +1,39 @@
 'use client';
 import React from 'react';
 import styles from './main.module.scss';
+
 interface CalendarHeaderProps {
   weekData: Date[];
 }
 
 const CalendarHeader: React.FC<CalendarHeaderProps> = ({ weekData }) => {
+  const isToday = (date: Date) => {
+    const today = new Date();
+    return (
+      date.getDate() === today.getDate() &&
+      date.getMonth() === today.getMonth() &&
+      date.getFullYear() === today.getFullYear()
+    );
+  };
+
   return (
     <div className={styles.calendarHeaderContainerMain}>
       <div className={styles.nameContainer}>
-        {/* <p>IMIE I NAZWISKO</p> */}
       </div>
       <div className={styles.calendarDayContainer}>
-      {weekData.map((day, index) => (
-        <div key={index} className={styles.dayContainer}>
-          <div className={styles.dayName}>
-            {day.toLocaleDateString('pl-PL', { weekday: 'long' })}
+        {weekData.map((day, index) => (
+          <div
+            key={index}
+            className={`${styles.dayContainer} ${isToday(day) ? styles.today : ''}`}
+          >
+            <div className={styles.dayName}>
+              {day.toLocaleDateString('pl-PL', { weekday: 'long' })}
+            </div>
+            <div className={styles.dayDate}>
+              {day.toLocaleDateString('pl-PL', { day: '2-digit', month: '2-digit' })}
+            </div>
           </div>
-          <div className={styles.dayDate}>
-            {day.toLocaleDateString('pl-PL', { day: '2-digit', month: '2-digit' })}
-          </div>
-        </div>
-      ))}
+        ))}
       </div>
     </div>
   );
