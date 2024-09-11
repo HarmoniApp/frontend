c6574d9201f5b96cc80f1f8bc7ed5d47f0325af3
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-09-11 11:52:45 +0200

    fix: adding functionality to the delete button popUp.

--- components/settings/employer/popUps/deleteConfirmation/index.tsx
+++ components/settings/employer/popUps/deleteConfirmation/index.tsx
@@ -5,6 +5,12 @@ interface DeleteConfirmationProps {
 }
 
 const DeleteConfirmation:React.FC <DeleteConfirmationProps> = ({onClose, onDelete, info}) => {
+
+    const handleDelete = () => {
+        onDelete();
+        onClose();
+    }
+    // console.log("Render DeleteConfirmation for:", info);
     return (
         <div className="delete-confirmation">
             <div className="delete-confirmation__content">
@@ -12,7 +18,7 @@ const DeleteConfirmation:React.FC <DeleteConfirmationProps> = ({onClose, onDelet
                 <p className="delete-confirmation__text">Are you sure you want to delete: {info}?</p>
                 <div className="delete-confirmation__buttons">
                     <button className="delete-confirmation__button" onClick={onClose}>Cancel</button>
-                    <button className="delete-confirmation__button" onClick={onDelete}>Delete</button>
+                    <button className="delete-confirmation__button" onClick={handleDelete}>Delete</button>
                 </div>
             </div>
         </div>
