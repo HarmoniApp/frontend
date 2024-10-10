d96f1fd36d0ec87589f75a3f193dd4039ae03352
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-10-10 08:54:08 +0200

    the display of popUp notifications with their basic SCSS styles has been added.

--- components/navbar/top/notifications/index.tsx
+++ components/navbar/top/notifications/index.tsx
@@ -1,19 +1,38 @@
 'use client';
 import React from "react";
-import Notification from "@/components/types/notification";
+import Notification from '@/components/types/notification';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 import styles from "./main.module.scss";
-
 interface NotificationsProps {
-    userId: number;
+    notifications: Notification[];
     onClose: () => void;
+    markAsRead: (id: number) => void;
 }
 
-const Notifications: React.FC<NotificationsProps> = ({userId, onClose}) => {
-   
+const Notifications: React.FC<NotificationsProps> = ({ notifications, onClose, markAsRead }) => {
     return (
-        <div className={styles.notificationContainerMain}>
-            <p>User ID: {userId}</p>
-            <button onClick={onClose}>Zamknij</button>
+        <div className={styles.notificationsPopup}>
+            <div className={styles.popupHeader}>
+                <label className={styles.notificationLabel}>Powiadomienia</label>
+                <button onClick={onClose} className={styles.backButton}><FontAwesomeIcon icon={faX} className={styles.icon}/></button> 
+            </div>
+            {notifications.length > 0 ? (
+                notifications.map(notification => (
+                    <div
+                        key={notification.id}
+                        className={`${styles.notificationItem} ${notification.read ? '' : styles.unread}`}
+                        onClick={() => markAsRead(notification.id)}
+                    >
+                        <strong>{notification.title}</strong>
+                        <p>{notification.message}</p>
+                        <small>Type: {notification.type_name}</small><br />
+                        <small>Date: {new Date(notification.created_at).toLocaleString()}</small>
+                    </div>
+                ))
+            ) : (
+                <p>No Notifications</p>
+            )}
         </div>
     );
 };
