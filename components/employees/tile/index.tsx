f8d2473db9c86dc0c28195ff83632b693dc9a407
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-10-16 13:36:54 +0200

    feat: add tooltip for language flags.

--- components/employees/tile/index.tsx
+++ components/employees/tile/index.tsx
@@ -5,7 +5,7 @@ import PersonTile from '@/components/types/personTile';
 import Flag from 'react-flagkit';
 interface LanguageTileProps {
   person: PersonTile;
-  view: 'tiles' | 'list'; 
+  view: 'tiles' | 'list';
 }
 
 const Tile: React.FC<LanguageTileProps> = ({ person, view }) => {
@@ -17,19 +17,43 @@ const Tile: React.FC<LanguageTileProps> = ({ person, view }) => {
 
   const tileClassName = view === 'tiles' ? styles.tileContainerMain : styles.listContainerMain;
 
+  const maxVisibleFlags = 2;
+
+  const visibleLanguages = person.languages.length > maxVisibleFlags 
+    ? person.languages.slice(0, 1)
+    : person.languages.slice(0, maxVisibleFlags);
+  
+  const hiddenLanguages = person.languages.length > maxVisibleFlags 
+    ? person.languages.slice(1) 
+    : []; 
+
   return (
     <div onClick={handleClick} className={tileClassName}>
       <div className={styles.employeeImageContainer}>
-        <img className={styles.employeeImage} src={`http://localhost:8080/api/v1/userPhoto/${person.photo}`} alt="User Photo" />  
+        <img className={styles.employeeImage} src={`http://localhost:8080/api/v1/userPhoto/${person.photo}`} alt="User Photo" />
       </div>
       <div className={styles.fullNameContainer}>
         <label className={styles.fullNameParagraph}>{person.firstname}</label>
         <label className={styles.fullNameParagraph}>{person.surname}</label>
       </div>
       <div className={styles.languagesContainer}>
-        {person.languages.map((language, index) => (
+        {visibleLanguages.map((language, index) => (
           <Flag key={index} className={styles.languageFlag} country={language.code.toUpperCase()} />
         ))}
+
+        {hiddenLanguages.length > 0 && (
+          <div className={styles.moreFlagsTooltip}>
+            +{hiddenLanguages.length}
+            <div className={styles.tooltipContent}
+            style={{
+              left: `${-20 * hiddenLanguages.length}px`
+            }}>
+              {hiddenLanguages.map((language, index) => (
+                <Flag key={index} className={styles.languageFlag} country={language.code.toUpperCase()} />
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
