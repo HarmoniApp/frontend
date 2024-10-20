0d9ddaeea2ac932f437d203901557edf832a82df
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-10-20 17:51:42 +0200

    fix: fetch absences from user.

--- components/variables/index.tsx
+++ components/variables/index.tsx
@@ -1,5 +1,5 @@
-// export const loggedUserID = 26;
-// export const loggedUserRoleAdmin = false;
+export const loggedUserID = 26;
+export const loggedUserRoleAdmin = false;
 
-export const loggedUserID = 1;
-export const loggedUserRoleAdmin = true;
+// export const loggedUserID = 1;
+// export const loggedUserRoleAdmin = true;
