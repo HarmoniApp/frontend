d02bdf12b59dc915c466131d9e00ca12a53dc0a3
MarMarcz <s24566@pjwstk.edu.pl>
2024-11-25 19:00:52 +0100

    refactor: move sidebar in chat to another file

--- components/chat/index.tsx
+++ components/chat/index.tsx
@@ -1,17 +1,15 @@
 "use client";
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCommentDots, faPaperPlane, faImage, faPlus, faSearch, faEye, faUser, faUsers, faEdit, faUserMinus, faXmark, faTrash } from '@fortawesome/free-solid-svg-icons';
 import styles from './main.module.scss';
 import Message from '@/components/types/message';
 import Language from '@/components/types/language';
 import ChatPartner from '@/components/types/chatPartner';
-import AuthorizedImage from '@/components/chat/authorizedImage';
 import EditGroup from '@/components/chat/editGroup';
 import SearchUser from '@/components/chat/searchUser';
 import SendMessageForm from '@/components/chat/sendMessageForm';
 import CreateGroupChatForm from '@/components/chat/createGroupChatForm';
 import Conversation from '@/components/chat/conversation';
+import Sidebar from '@/components/chat/sidebar';
 import { Client } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import { ProgressSpinner } from 'primereact/progressspinner';
@@ -35,21 +33,6 @@ const Chat = () => {
       setUserId(Number(storedUserId));
     }
   }, []);
-
-  const handleNewIndividualChat = () => {
-    setLoading(true);
-    setNewChat(true);
-    setSelectedChat(null);
-    setLoading(false);
-  };
-
-  const handleNewGroupChat = () => {
-    setLoading(true);
-    setNewChat(true);
-    setChatType('group');
-    setSelectedChat(null);
-    setLoading(false);
-  };
 
   const handleSelectUser = (user: ChatPartner) => {
     setLoading(true);
@@ -82,17 +65,6 @@ const Chat = () => {
     fetchLanguages();
     setLoading(false);
   }, []);
-
-  const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setLoading(true);
-    const language = event.target.value;
-    setSelectedLanguage(language);
-
-    if (selectedChat) {
-      fetchChatHistory(selectedChat, language);
-    }
-    setLoading(false);
-  };
 
   useEffect(() => {
     // setLoading(true);
@@ -423,116 +395,58 @@ const Chat = () => {
       {userId !== 0 ? (
         <>
           <div className={styles.sidebar}>
-            <div className={styles.sidebarHeader}>
-              <div className={styles.headerTop}>
-                <span>Translate: </span>
-                <select
-                  value={selectedLanguage}
-                  onChange={handleLanguageChange}
-                  className={styles.languageSelect}
-                >
-                  <option value="">None</option>
-                  {languages.map((language) => (
-                    <option key={language.code} value={language.code}>
-                      {language.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className={styles.headerBottom}>
-                <div className={styles.sectionSelector}>
-                  <div
-                    onClick={() => setChatType('user')}
-                    className={`${styles.sectionBlock} ${chatType === 'user' ? styles.activeSection : ''}`}
-                  >
-                    <FontAwesomeIcon icon={faUser} /> Individual
-                  </div>
-
-                  <div
-                    onClick={() => setChatType('group')}
-                    className={`${styles.sectionBlock} ${chatType === 'group' ? styles.activeSection : ''}`}
-                  >
-                    <FontAwesomeIcon icon={faUsers} /> Groups
-                  </div>
-                </div>
-
-              </div>
-              {chatType === 'user' && (
-                <div>
-                  <FontAwesomeIcon icon={faPlus} className={styles.icon} onClick={handleNewIndividualChat} />
-                  New individual
-                </div>
-              )}
-
-              {chatType === 'group' && (
-                <div>
-                  <FontAwesomeIcon icon={faPlus} className={styles.icon} onClick={handleNewGroupChat} />
-                  New group
-                </div>
-              )}
-            </div>
-            <ul className={styles.chatList}>
-              {chatPartners.map((partner) => (
-                <li
-                  key={partner.id}
-                  className={`${styles.chatItem} ${selectedChat === partner ? styles.activeChat : ''}`}
-                  onClick={() => fetchChatHistory(partner, selectedLanguage)}
-                >
-                  {partner.photo ? (
-                    <AuthorizedImage
-                      src={`${process.env.NEXT_PUBLIC_API_URL}/api/v1/userPhoto/${partner.photo}`}
-                    />
-                  ) : (
-                    <FontAwesomeIcon icon={faUsers} className={styles.defaultAvatarIcon} />
-                  )}
-                  <div>
-                    <p className={styles.chatName}>{partner.name}</p>
-                    <p className={styles.lastMessage}>{partner.lastMessage || 'Brak wiadomości'}</p>
-                  </div>
-                </li>
-              ))}
-            </ul>
+            <Sidebar selectedLanguage={selectedLanguage}
+              setSelectedLanguage={setSelectedLanguage}
+              languages={languages}
+              selectedChat={selectedChat}
+              setSelectedChat={setSelectedChat}
+              setNewChat={setNewChat}
+              chatType={chatType}
+              setChatType={setChatType}
+              chatPartners={chatPartners}
+              loading={setLoading}
+              fetchChatHistory={fetchChatHistory} />
           </div>
           <div className={styles.chatWindow}>
             {newChat ? (
               chatType === 'group' ? (
                 <div className={styles.newChat}>
-                  <CreateGroupChatForm 
-                  userId={userId} 
-                  setChatType={setChatType} 
-                  setNewChat={setNewChat} 
-                  chatPartners={chatPartners}
-                  setChatPartners={setChatPartners}
-                  setSelectedChat={setSelectedChat}
-                  fetchChatHistory={fetchChatHistory}
-                  loadChatPartnersGroups={loadChatPartnersGroups}
-                  loading={setLoading}
+                  <CreateGroupChatForm
+                    userId={userId}
+                    setChatType={setChatType}
+                    setNewChat={setNewChat}
+                    chatPartners={chatPartners}
+                    setChatPartners={setChatPartners}
+                    setSelectedChat={setSelectedChat}
+                    fetchChatHistory={fetchChatHistory}
+                    loadChatPartnersGroups={loadChatPartnersGroups}
+                    loading={setLoading}
                   />
                 </div>
               ) : (
                 <div className={styles.newChat}>
-                  <SearchUser handleSelectUser={handleSelectUser}  groupChat={false}/>
+                  <SearchUser handleSelectUser={handleSelectUser} groupChat={false} />
                 </div>)
             ) : selectedChat ? (
               <>
-                <Conversation 
-                userId={userId} 
-                messages={messages} 
-                chatType={chatType} 
-                selectedChat={selectedChat} 
-                setIsEditGroupModalOpen={setIsEditGroupModalOpen}
+                <Conversation
+                  userId={userId}
+                  messages={messages}
+                  chatType={chatType}
+                  selectedChat={selectedChat}
+                  setIsEditGroupModalOpen={setIsEditGroupModalOpen}
                 />
 
-                <SendMessageForm 
-                selectedChat={selectedChat} 
-                setSelectedChat={setSelectedChat} 
-                messages={messages} 
-                setMessages={setMessages}
-                userId={userId}
-                loadChatPartnersIndividual={loadChatPartnersIndividual}
-                loadChatPartnersGroups={loadChatPartnersGroups}
-                selectedLanguage={selectedLanguage}
-                loading={setLoading}
+                <SendMessageForm
+                  selectedChat={selectedChat}
+                  setSelectedChat={setSelectedChat}
+                  messages={messages}
+                  setMessages={setMessages}
+                  userId={userId}
+                  loadChatPartnersIndividual={loadChatPartnersIndividual}
+                  loadChatPartnersGroups={loadChatPartnersGroups}
+                  selectedLanguage={selectedLanguage}
+                  loading={setLoading}
                 />
               </>
             ) : (
@@ -542,12 +456,12 @@ const Chat = () => {
         </>
       ) : (<div className={styles.spinnerContainer}><ProgressSpinner /></div>)}
       {isEditGroupModalOpen && (
-        <EditGroup 
-        editGroupModal={setIsEditGroupModalOpen} 
-        selectedUsers={selectedUsers} 
-        setSelectedUsers={setSelectedUsers} 
-        selectedChat={selectedChat} 
-        loading={setLoading}/>
+        <EditGroup
+          editGroupModal={setIsEditGroupModalOpen}
+          selectedUsers={selectedUsers}
+          setSelectedUsers={setSelectedUsers}
+          selectedChat={selectedChat}
+          loading={setLoading} />
       )}
       {loading && (
         <div className={styles.loadingModalOverlay}>
---
+++ components/chat/sidebar/index.tsx
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import ChatPartner from '@/components/types/chatPartner';
+import AuthorizedImage from '@/components/chat/authorizedImage';
+import Language from '@/components/types/language';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers, faUser, faPlus } from '@fortawesome/free-solid-svg-icons';
+import styles from './main.module.scss';
+
+interface SidebarProps {
+    selectedLanguage: string;
+    setSelectedLanguage: (setSelectedLanguage: string) => void;
+    languages: Language[];
+    selectedChat: ChatPartner | null;
+    setSelectedChat: (chatPartner: ChatPartner | null) => void;
+    setNewChat: (newChat: boolean) => void;
+    chatType: 'user' | 'group';
+    setChatType: (chatType: 'user' | 'group') => void;
+    chatPartners: ChatPartner[];
+    loading: (loading: boolean) => void;
+    fetchChatHistory: (selectedChat: ChatPartner, language: string) => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ selectedLanguage, setSelectedLanguage, languages, selectedChat, setSelectedChat, setNewChat, chatType, setChatType, chatPartners, loading, fetchChatHistory }) => {
+    const handleNewIndividualChat = () => {
+        loading(true);
+        setNewChat(true);
+        setSelectedChat(null);
+        loading(false);
+    };
+
+    const handleNewGroupChat = () => {
+        loading(true);
+        setNewChat(true);
+        setChatType('group');
+        setSelectedChat(null);
+        loading(false);
+    };
+
+    const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        loading(true);
+        const language = event.target.value;
+        setSelectedLanguage(language);
+
+        if (selectedChat) {
+            fetchChatHistory(selectedChat, language);
+        }
+        loading(false);
+    };
+
+    return (
+        <>
+            <div className={styles.sidebarHeader}>
+                <div className={styles.headerTop}>
+                    <span>Translate: </span>
+                    <select
+                        value={selectedLanguage}
+                        onChange={handleLanguageChange}
+                        className={styles.languageSelect}
+                    >
+                        <option value="">None</option>
+                        {languages.map((language) => (
+                            <option key={language.code} value={language.code}>
+                                {language.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className={styles.headerBottom}>
+                    <div className={styles.sectionSelector}>
+                        <div
+                            onClick={() => setChatType('user')}
+                            className={`${styles.sectionBlock} ${chatType === 'user' ? styles.activeSection : ''}`}
+                        >
+                            <FontAwesomeIcon icon={faUser} /> Individual
+                        </div>
+
+                        <div
+                            onClick={() => setChatType('group')}
+                            className={`${styles.sectionBlock} ${chatType === 'group' ? styles.activeSection : ''}`}
+                        >
+                            <FontAwesomeIcon icon={faUsers} /> Groups
+                        </div>
+                    </div>
+
+                </div>
+                {chatType === 'user' && (
+                    <div>
+                        <FontAwesomeIcon icon={faPlus} className={styles.icon} onClick={handleNewIndividualChat} />
+                        New individual
+                    </div>
+                )}
+
+                {chatType === 'group' && (
+                    <div>
+                        <FontAwesomeIcon icon={faPlus} className={styles.icon} onClick={handleNewGroupChat} />
+                        New group
+                    </div>
+                )}
+            </div>
+            <ul className={styles.chatList}>
+                {chatPartners.map((partner) => (
+                    <li
+                        key={partner.id}
+                        className={`${styles.chatItem} ${selectedChat === partner ? styles.activeChat : ''}`}
+                        onClick={() => fetchChatHistory(partner, selectedLanguage)}
+                    >
+                        {partner.photo ? (
+                            <AuthorizedImage
+                                src={`${process.env.NEXT_PUBLIC_API_URL}/api/v1/userPhoto/${partner.photo}`}
+                            />
+                        ) : (
+                            <FontAwesomeIcon icon={faUsers} className={styles.defaultAvatarIcon} />
+                        )}
+                        <div>
+                            <p className={styles.chatName}>{partner.name}</p>
+                            <p className={styles.lastMessage}>{partner.lastMessage || 'Brak wiadomości'}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default Sidebar;
