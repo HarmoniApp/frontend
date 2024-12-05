b174b3678a2662078cb1a467d99e24c36ab47275
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-12-05 09:06:08 +0100

    fix: loading notifications and photo on navbar in app/dashboard.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,12 @@
 'use client';
 import React, { useState } from "react";
-import NavbarTop from "@/components/navbar/top";
+import Navbar from "@/components/navbar";
 import DashboardCenter from "@/components/dashboard";
-import Account from "@/components/account";
 
 export default function Dashboard() {
-
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
-
     return (
         <div>
-            <NavbarTop onAccountIconClick={toggleSidebar}/>
-            <Account isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+            <Navbar />
             <DashboardCenter />
         </div>
     )
