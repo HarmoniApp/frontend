a619660343fc4dcc957fa97c33e20a6470b4140a
MarMarcz <s24566@pjwstk.edu.pl>
2024-12-11 10:26:59 +0100

    refactor: move post message to service

--- services/chatService.ts
+++ services/chatService.ts
@@ -1,4 +1,34 @@
+import Message from "@/components/types/message";
 import { fetchCsrfToken } from "./csrfService";
+
+export const posChattMessage = async (
+    messageData: any,
+    setMessages: (messages: Message[]) => void,
+    messages: Message[]): Promise<void> => {
+
+    try {
+        const tokenXSRF = await fetchCsrfToken();
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/message/send`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
+                'X-XSRF-TOKEN': tokenXSRF,
+            },
+            credentials: 'include',
+            body: JSON.stringify(messageData),
+        });
+        if (!response.ok) {
+            console.error('Failed to send message:', response.statusText);
+            throw new Error('Failed to send message');
+        }
+        const data = await response.json();
+        setMessages([...messages, data]);
+    } catch (error) {
+        console.error(`Error sending message`, error);
+    }
+};
 
 export const deleteGroup = async (
     id: number,
