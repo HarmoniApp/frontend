290bd477e56fb21d1cdb6f070c02cc8be55acba6
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-12-16 20:55:00 +0100

    feat: added tostify to PDF and XLSX genarate.

--- services/pdfService.ts
+++ services/pdfService.ts
@@ -1,59 +1,79 @@
-export const downloadSchedulePdf = async (
-    currentWeek: Date[]): Promise<void> => {
-    try {
-        const startOfWeek = currentWeek[0].toISOString().split('T')[0];
-        const endOfWeek = currentWeek[6].toISOString().split('T')[0];
-        const responsePDF = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/pdf/generate-pdf-shift?startOfWeek=${startOfWeek}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
-            },
-        }
-        );
+import { toast } from "react-toastify";
 
-        if (!responsePDF.ok) {
-            console.error('Error downloading PDF');
-            return;
+export const downloadSchedulePdf = async (currentWeek: Date[]): Promise<void> => {
+    const startOfWeek = currentWeek[0].toISOString().split('T')[0];
+    const endOfWeek = currentWeek[6].toISOString().split('T')[0];
+
+    await toast.promise(
+        (async () => {
+            try {
+                const responsePDF = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/pdf/generate-pdf-shift?startOfWeek=${startOfWeek}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
+                    },
+                });
+
+                if (!responsePDF.ok) {
+                    const errorResponse = await responsePDF.text();
+                    throw new Error(errorResponse || 'Wystąpił błąd podczas pobierania pliku PDF.');
+                }
+
+                const blob = await responsePDF.blob();
+                const url = window.URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                const filename = `grafik_${startOfWeek} - ${endOfWeek}.pdf`;
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+            } catch (error) {
+                console.error('Error downloading schedule PDF: ', error);
+                throw error;
+            }
+        })(),
+        {
+            pending: 'Pobieranie grafiku...',
+            success: `Grafik ${startOfWeek} - ${endOfWeek} został pobrany!`
         }
-        const blob = await responsePDF.blob();
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        const filename = `grafik_${startOfWeek} - ${endOfWeek}.pdf`;
-        link.setAttribute('download', filename);
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    );
 };
 
 export const downloadUsersPDF = async (): Promise<void> => {
-    try {
-        const responsePDF = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/pdf/generate-pdf-all-employees`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/pdf',
-                'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
-            }
-        });
+    await toast.promise(
+        (async () => {
+            try {
+                const responsePDF = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/pdf/generate-pdf-all-employees`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/pdf',
+                        'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
+                    },
+                });
 
-        if (!responsePDF.ok) {
-            console.error('Error downloading PDF');
-            return;
+                if (!responsePDF.ok) {
+                    const errorResponse = await responsePDF.text();
+                    throw new Error(errorResponse || 'Wystąpił błąd podczas pobierania pliku PDF.');
+                }
+                const blob = await responsePDF.blob();
+                const url = window.URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                const filename = `pracownicy.pdf`;
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+            } catch (error) {
+                console.error('Error downloading users PDF: ', error);
+                throw error;
+            }
+        })(),
+        {
+            pending: 'Pobieranie listy pracowników...',
+            success: 'Lista pracowników została pobrana!'
         }
-        const blob = await responsePDF.blob();
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        const filename = `pracownicy.pdf`;
-        link.setAttribute('download', filename);
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    );
 };
--- services/xlsxService.ts
+++ services/xlsxService.ts
@@ -1,59 +1,82 @@
-export const downloadScheduleXLSX = async (
-    currentWeek: Date[]): Promise<void> => {
-    try {
-        const startOfWeek = currentWeek[0].toISOString().split('T')[0];
-        const endOfWeek = currentWeek[6].toISOString().split('T')[0];
-        const responseXLSX = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/excel/shifts/export-excel?start=${startOfWeek}&end=${endOfWeek}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
-            },
-        }
-        );
+import { toast } from "react-toastify";
 
-        if (!responseXLSX.ok) {
-            console.error('Error downloading XLSX');
-            return;
+export const downloadScheduleXLSX = async (
+    currentWeek: Date[]
+): Promise<void> => {
+    const startOfWeek = currentWeek[0].toISOString().split('T')[0];
+    const endOfWeek = currentWeek[6].toISOString().split('T')[0];
+
+    await toast.promise(
+        (async () => {
+            try {
+                const responseXLSX = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/excel/shifts/export-excel?start=${startOfWeek}&end=${endOfWeek}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
+                    },
+                });
+
+                if (!responseXLSX.ok) {
+                    const errorResponse = await responseXLSX.text();
+                    throw new Error(errorResponse || 'Wystąpił błąd podczas pobierania pliku XLSX.');
+                }
+
+                const blob = await responseXLSX.blob();
+                const url = window.URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                const filename = `grafik_${startOfWeek} - ${endOfWeek}.xlsx`;
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+            } catch (error) {
+                console.error('Error downloading schedule XLSX: ', error);
+                throw error;
+            }
+        })(),
+        {
+            pending: 'Pobieranie grafiku w formacie XLSX...',
+            success: `Grafik ${startOfWeek} - ${endOfWeek} został pobrany!`
         }
-        const blob = await responseXLSX.blob();
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        const filename = `grafik_${startOfWeek} - ${endOfWeek}.xlsx`;
-        link.setAttribute('download', filename);
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    );
 };
 
 export const downloadUsersXLSX = async (): Promise<void> => {
-    try {
-        const responseXLSX = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/excel/users/export-excel`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/pdf',
-                'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
-            }
-        });
+    await toast.promise(
+        (async () => {
+            try {
+                const responseXLSX = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/excel/users/export-excel`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('tokenJWT')}`,
+                    },
+                });
 
-        if (!responseXLSX.ok) {
-            console.error('Error downloading XLSX');
-            return;
+                if (!responseXLSX.ok) {
+                    const errorResponse = await responseXLSX.text();
+                    throw new Error(errorResponse || 'Wystąpił błąd podczas pobierania pliku XLSX.');
+                }
+
+                const blob = await responseXLSX.blob();
+                const url = window.URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                const filename = `pracownicy.xlsx`;
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+            } catch (error) {
+                console.error('Error downloading users XLSX: ', error);
+                throw error;
+            }
+        })(),
+        {
+            pending: 'Pobieranie listy pracowników w formacie XLSX...',
+            success: 'Lista pracowników została pobrana!'
         }
-        const blob = await responseXLSX.blob();
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        const filename = `pracownicy.xlsx`;
-        link.setAttribute('download', filename);
-        document.body.appendChild(link);
-        link.click();
-        link.remove();
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    );
 };
