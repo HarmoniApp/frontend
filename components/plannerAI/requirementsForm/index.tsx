83efbb53ebb6da3f7654d4b8e86fd697359d320a
MarMarcz <s24566@pjwstk.edu.pl>
2024-12-20 16:07:40 +0100

    refactor: add custom hooks to requiremnts form in planerAi

--- components/plannerAI/requirementsForm/index.tsx
+++ components/plannerAI/requirementsForm/index.tsx
@@ -1,111 +1,33 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Formik, Form, Field, ErrorMessage, FormikProps } from 'formik';
-import PredefinedShift from '@/components/types/predefinedShifts';
-import Role from '@/components/types/role';
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import Instruction from '@/components/plannerAI/instruction';
-import IRequirementsForm from '@/components/types/requirementsForm';
 import styles from './main.module.scss';
-import { fetchRoles } from '@/services/roleService';
-import { fetchPredefinedShifts } from '@/services/predefineShiftService';
 import LoadingSpinner from '@/components/loadingSpinner';
-import { generateScheduleAi, revokeScheduleAi } from '@/services/planerAiService';
 import CustomButton from '@/components/customButton';
 import classNames from 'classnames';
 import { planerAiValidationSchema } from '@/validationSchemas/planerAiValidationSchema';
 import ConfirmationPopUp from '@/components/confirmationPopUp';
 import PredefineShiftsAndRoles from './preShiftsAndRoles';
+import useRequirementsForm from '@/hooks/useRequirementsForm';
 
 const RequirementsForm: React.FC = () => {
-    const [forms, setForms] = useState<IRequirementsForm[]>([
-        { id: Date.now(), date: '', shifts: [] },
-    ]);
-
-    const [predefineShifts, setPredefineShifts] = useState<PredefinedShift[]>([]);
-    const [roles, setRoles] = useState<Role[]>([]);
-    const [loading, setLoading] = useState(false);
     const [isInstructionOpen, setIsInstructionOpen] = useState(false);
-    const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
-    const formRefs = useRef<FormikProps<any>[]>([]);
 
-    useEffect(() => {
-        const loadData = async () => {
-            setLoading(true);
-            await fetchPredefinedShifts(setPredefineShifts);
-            await fetchRoles(setRoles);
-            setLoading(false);
-        };
-
-        loadData();
-    }, []);
-
-    const handleAddForm = () => {
-        setForms((prevForms) => [
-            ...prevForms,
-            { id: Date.now(), date: '', shifts: [] },
-        ]);
-    };
-
-    const handleRemoveForm = (id: number) => {
-        setForms((prevForms) => prevForms.filter((form) => form.id !== id));
-        formRefs.current = formRefs.current.filter((ref, index) => forms[index]?.id !== id);
-    };
-
-    const handleSubmit = async () => {
-        const validationPromises = formRefs.current.map(async (formRef) => {
-            await formRef.setTouched({
-                date: true,
-                shifts: true,
-                // roles: true,
-            });
-
-            return formRef.validateForm();
-        });
-
-        const validationResults = await Promise.all(validationPromises);
-        const hasErrors = validationResults.some((errors) => Object.keys(errors).length > 0);
-        if (hasErrors) {
-            console.error('Niektóre formularze zawierają błędy:', validationResults);
-            return;
-        }
-
-        const payload = forms.map((form) => ({
-            date: form.date,
-            shifts: form.shifts.map((shift) => ({
-                shiftId: shift.shiftId,
-                roles: shift.roles.map((role) => ({
-                    roleId: role.roleId,
-                    quantity: role.quantity,
-                })),
-            })),
-        }));
-
-        try {
-            await generateScheduleAi(payload);
-
-            formRefs.current.forEach((formRef) => {
-                formRef.resetForm();
-            });
-        } catch (error) {
-            console.error('Wystąpił błąd przy wysyłaniu danych:', error);
-        }
-    };
-
-    const handleRevoke = async () => {
-        try {
-            setIsConfirmationModalOpen(false);
-            await revokeScheduleAi();
-        } catch (error) {
-            console.error('Failed to send data:', error);
-        }
-    };
-
-    const handleEditDate = (formId: number, value: string) => {
-        setForms((prevForms) =>
-            prevForms.map((form) =>
-                form.id === formId ? { ...form, date: value } : form
-            )
-        );
-    };
+    const {
+        forms,
+        setForms,
+        predefineShifts,
+        roles,
+        loading,
+        isConfirmationModalOpen,
+        setIsConfirmationModalOpen,
+        formRefs,
+        handleAddForm,
+        handleRemoveForm,
+        handleEditDate,
+        handleSubmit,
+        handleRevoke,
+    } = useRequirementsForm();
 
     return (
         <div className={styles.planerAiContainer}>
@@ -147,7 +69,7 @@ const RequirementsForm: React.FC = () => {
                                         : 'Musisz wybrać przynajmniej jedną rolę'}
                                 </div>
                             )}
-                            <PredefineShiftsAndRoles 
+                            <PredefineShiftsAndRoles
                                 predefineShifts={predefineShifts}
                                 roles={roles}
                                 values={values}
---
+++ hooks/useRequirementsForm.ts
@@ -0,0 +1,115 @@
+import PredefinedShift from "@/components/types/predefinedShifts";
+import IRequirementsForm from "@/components/types/requirementsForm";
+import Role from "@/components/types/role";
+import { generateScheduleAi, revokeScheduleAi } from "@/services/planerAiService";
+import { fetchPredefinedShifts } from "@/services/predefineShiftService";
+import { fetchRoles } from "@/services/roleService";
+import { FormikProps } from "formik";
+import { useEffect, useRef, useState } from "react";
+
+const useRequirementsForm = () => {
+    const [forms, setForms] = useState<IRequirementsForm[]>([
+        { id: Date.now(), date: '', shifts: [] },
+    ]);
+    const [predefineShifts, setPredefineShifts] = useState<PredefinedShift[]>([]);
+    const [roles, setRoles] = useState<Role[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
+    const formRefs = useRef<FormikProps<any>[]>([]);
+
+    useEffect(() => {
+        const loadData = async () => {
+            setLoading(true);
+            await fetchPredefinedShifts(setPredefineShifts);
+            await fetchRoles(setRoles);
+            setLoading(false);
+        };
+        loadData();
+    }, []);
+
+    const handleAddForm = () => {
+        setForms((prevForms) => [
+            ...prevForms,
+            { id: Date.now(), date: '', shifts: [] },
+        ]);
+    };
+
+    const handleRemoveForm = (id: number) => {
+        setForms((prevForms) => prevForms.filter((form) => form.id !== id));
+        formRefs.current = formRefs.current.filter((ref, index) => forms[index]?.id !== id);
+    };
+
+    const handleSubmit = async () => {
+        const validationPromises = formRefs.current.map(async (formRef) => {
+            await formRef.setTouched({
+                date: true,
+                shifts: true,
+            });
+
+            return formRef.validateForm();
+        });
+
+        const validationResults = await Promise.all(validationPromises);
+        const hasErrors = validationResults.some((errors) => Object.keys(errors).length > 0);
+        if (hasErrors) {
+            console.error('Niektóre formularze zawierają błędy:', validationResults);
+            return;
+        }
+
+        const payload = forms.map((form) => ({
+            date: form.date,
+            shifts: form.shifts.map((shift) => ({
+                shiftId: shift.shiftId,
+                roles: shift.roles.map((role) => ({
+                    roleId: role.roleId,
+                    quantity: role.quantity,
+                })),
+            })),
+        }));
+
+        try {
+            await generateScheduleAi(payload);
+
+            formRefs.current.forEach((formRef) => {
+                formRef.resetForm();
+            });
+        } catch (error) {
+            console.error('Wystąpił błąd przy wysyłaniu danych:', error);
+        }
+    };
+
+    const handleRevoke = async () => {
+        try {
+            setIsConfirmationModalOpen(false);
+            await revokeScheduleAi();
+        } catch (error) {
+            console.error('Failed to send data:', error);
+        }
+    };
+
+    const handleEditDate = (formId: number, value: string) => {
+        setForms((prevForms) =>
+            prevForms.map((form) =>
+                form.id === formId ? { ...form, date: value } : form
+            )
+        );
+    };
+
+    return {
+        forms,
+        setForms,
+        predefineShifts,
+        roles,
+        loading,
+        isConfirmationModalOpen,
+        setIsConfirmationModalOpen,
+        formRefs,
+        handleAddForm,
+        handleRemoveForm,
+        handleEditDate,
+        handleSubmit,
+        handleRevoke,
+    };
+}
+
+export default useRequirementsForm;
