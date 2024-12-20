6de3f974341a85bf1023c834e81b21d95a2e4c10
MarMarcz <s24566@pjwstk.edu.pl>
2024-12-20 16:18:01 +0100

    refactor: add custom hooks to login

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,85 +1,23 @@
 'use client';
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import styles from './main.module.scss';
-import { jwtDecode } from "jwt-decode";
-import MyJwtPayload from '@/components/types/myJwtPayload';
 import LoadingSpinner from '../loadingSpinner';
-import { patchChangePassword } from '@/services/passwordService';
 import { changePasswordSchema, loginValidationSchema } from '@/validationSchemas/loginValidationSchema';
+import useLogin from '@/hooks/useLogin';
 
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [loginError, setLoginError] = useState<string | null>(null);
-  const [isChangePasswordModalOpen, setIsChangePasswordModalOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [passwordPath, setPasswordPath] = useState<string>('');
-  const router = useRouter();
-
-  const handleSubmit = async (values: any) => {
-    setLoading(true);
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: values.email,
-          password: values.password,
-        }),
-      });
-
-      if (response.ok) {
-        setLoginError(null);
-        const data = await response.json();
-        const tokenJWT = data.jwtToken;
-        setPasswordPath(data.path);
-
-        const decodedToken = jwtDecode<MyJwtPayload>(tokenJWT);
-        const userId = decodedToken.id;
-        const username = decodedToken.username;
-        const isAdmin = decodedToken.authorities === 'ROLE_ADMIN';
-
-        sessionStorage.setItem('tokenJWT', tokenJWT);
-        sessionStorage.setItem('userId', userId.toString());
-        sessionStorage.setItem('username', username);
-        sessionStorage.setItem('isAdmin', JSON.stringify(isAdmin));
-
-        const oneTimeUsedPass = data.path || '';
-        if (oneTimeUsedPass.length > 0) {
-          setIsChangePasswordModalOpen(true);
-        } else {
-          if (isAdmin) {
-            router.push('/dashboard');
-          } else {
-            router.push('/schedule');
-          }
-        }
-      } else if (response.status === 401) {
-        setLoginError("Niepoprawne hasło lub login.");
-      } else {
-        setLoginError("Wystąpił błąd podczas logowania.");
-      }
-    } catch (error) {
-      console.error("An error occurred:", error);
-      setLoginError("Wystąpił błąd podczas logowania.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handlePasswordChangeSubmit = async (values: any) => {
-    try {
-      await patchChangePassword(values, passwordPath)
-      setIsChangePasswordModalOpen(false);
-    } catch (error) {
-      console.error("An error occurred while changing password:", error);
-    }
-  };
+  const {
+    showPassword,
+    setShowPassword,
+    loginError,
+    isChangePasswordModalOpen,
+    loading,
+    handleLogin,
+    handlePasswordChange,
+  } = useLogin();
 
   return (
     <div className={styles.container}>
@@ -87,7 +25,7 @@ const Login = () => {
       <Formik
         initialValues={{ email: '', password: '' }}
         validationSchema={loginValidationSchema}
-        onSubmit={handleSubmit}
+        onSubmit={handleLogin}
       >
         {({ errors, touched }) => (
           <Form className={styles.form}>
@@ -129,7 +67,7 @@ const Login = () => {
             <Formik
               initialValues={{ newPassword: '', repeatPassword: '' }}
               validationSchema={changePasswordSchema}
-              onSubmit={handlePasswordChangeSubmit}
+              onSubmit={handlePasswordChange}
             >
               {({ errors, touched }) => (
                 <Form>
