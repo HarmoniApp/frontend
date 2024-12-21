bd95dd26b90835eda30d851f08187e72632472af
MarMarcz <s24566@pjwstk.edu.pl>
2024-12-21 11:21:15 +0100

    refactor: add custom hooks to absenceCard

--- components/absence/employer/absenceCard/index.tsx
+++ components/absence/employer/absenceCard/index.tsx
@@ -1,71 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 import Absence from '@/components/types/absence';
-import AbsenceType from '@/components/types/absenceType';
-import User from "@/components/types/user";
 import styles from './main.module.scss';
-import { fetchSimpleUser } from '@/services/userService';
-import { fetchAbsenceType, patchAbsence } from '@/services/absenceService';
 import LoadingSpinner from '@/components/loadingSpinner';
 import ConfirmationPopUp from '@/components/confirmationPopUp';
+import useAbsenceCard from '@/hooks/useAbsenceCard';
 interface AbsenceCardProps {
     absence: Absence;
     onStatusUpdate: () => void;
 }
 
 const AbsenceCardEmployer: React.FC<AbsenceCardProps> = ({ absence, onStatusUpdate }) => {
-    const [absenceType, setAbsenceType] = useState<AbsenceType | null>(null);
-    const [user, setUser] = useState<User | null>(null);
-    const [modalIsOpenCancelAbsence, setModalIsOpenCancelAbsence] = useState(false);
-    const [modalIsOpenAproveAbsence, setModalIsOpenAproveAbsence] = useState(false);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        const loadData = async () => {
-            setLoading(true);
-            await fetchSimpleUser(absence.user_id, setUser, undefined);
-            await fetchAbsenceType(absence.absence_type_id, setAbsenceType);
-            setLoading(false);
-        };
-        loadData();
-
-    }, [absence.absence_type_id, absence.user_id]);
-
-    const subbmisionDate = () => new Date(absence.submission).toLocaleDateString();
-    const startDate = () => new Date(absence.start).toLocaleDateString();
-    const endDate = () => new Date(absence.end).toLocaleDateString();
-
-    const updateAbsenceStatus = async (absenceId: number, statusId: number) => {
-        setLoading(true);
-        try {
-            await patchAbsence(absenceId, statusId)
-        } catch (error) {
-            console.error('Error updating absence status:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handleDeclineClick = async () => {
-        try {
-            await updateAbsenceStatus(absence.id, 4);
-            setModalIsOpenCancelAbsence(false);
-            onStatusUpdate();
-        } catch (error) {
-            console.error('Error rejecting absence:', error);
-        }
-    };
-
-    const handleAcceptClick = async () => {
-        try {
-            await updateAbsenceStatus(absence.id, 2);
-            setModalIsOpenAproveAbsence(false);
-            onStatusUpdate();
-        } catch (error) {
-            console.error('Error approving absence:', error);
-        }
-    };
+    const {
+        absenceType,
+        user,
+        modalIsOpenCancelAbsence,
+        modalIsOpenAproveAbsence,
+        loading,
+        setModalIsOpenCancelAbsence,
+        setModalIsOpenAproveAbsence,
+        subbmisionDate,
+        startDate,
+        endDate,
+        handleDeclineClick,
+        handleAcceptClick,
+    } = useAbsenceCard(absence, onStatusUpdate);
 
     const renderButtons = () => {
         switch (absence.status.name) {
---
+++ hooks/useAbsenceCard.ts
@@ -0,0 +1,78 @@
+import absence from "@/components/absence";
+import Absence from "@/components/types/absence";
+import AbsenceType from "@/components/types/absenceType";
+import User from "@/components/types/user";
+import { fetchAbsenceType, patchAbsence } from "@/services/absenceService";
+import { fetchSimpleUser } from "@/services/userService";
+import { useState, useEffect } from "react";
+
+const useAbsenceCard = (absence: Absence, onStatusUpdate: () => void) => {
+    const [absenceType, setAbsenceType] = useState<AbsenceType | null>(null);
+    const [user, setUser] = useState<User | null>(null);
+    const [modalIsOpenCancelAbsence, setModalIsOpenCancelAbsence] = useState(false);
+    const [modalIsOpenAproveAbsence, setModalIsOpenAproveAbsence] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const loadData = async () => {
+            setLoading(true);
+            await fetchSimpleUser(absence.user_id, setUser, undefined);
+            await fetchAbsenceType(absence.absence_type_id, setAbsenceType);
+            setLoading(false);
+        };
+        loadData();
+
+    }, [absence.absence_type_id, absence.user_id]);
+
+    const subbmisionDate = () => new Date(absence.submission).toLocaleDateString();
+    const startDate = () => new Date(absence.start).toLocaleDateString();
+    const endDate = () => new Date(absence.end).toLocaleDateString();
+
+    const updateAbsenceStatus = async (absenceId: number, statusId: number) => {
+        setLoading(true);
+        try {
+            await patchAbsence(absenceId, statusId)
+        } catch (error) {
+            console.error('Error updating absence status:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleDeclineClick = async () => {
+        try {
+            await updateAbsenceStatus(absence.id, 4);
+            setModalIsOpenCancelAbsence(false);
+            onStatusUpdate();
+        } catch (error) {
+            console.error('Error rejecting absence:', error);
+        }
+    };
+
+    const handleAcceptClick = async () => {
+        try {
+            await updateAbsenceStatus(absence.id, 2);
+            setModalIsOpenAproveAbsence(false);
+            onStatusUpdate();
+        } catch (error) {
+            console.error('Error approving absence:', error);
+        }
+    };
+
+    return {
+        absenceType,
+        user,
+        modalIsOpenCancelAbsence,
+        modalIsOpenAproveAbsence,
+        loading,
+        setModalIsOpenCancelAbsence,
+        setModalIsOpenAproveAbsence,
+        subbmisionDate,
+        startDate,
+        endDate,
+        handleDeclineClick,
+        handleAcceptClick,
+    };
+}
+
+export default useAbsenceCard;
