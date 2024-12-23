169fcd499990dc926f8be96a2e0d67e903396f68
MarMarcz <s24566@pjwstk.edu.pl>
2024-12-23 11:48:47 +0100

    refactor: move useSchedule to customHook

--- components/schedule/employer/calendar/calendarRow/index.tsx
+++ components/schedule/employer/calendar/calendarRow/index.tsx
@@ -1,20 +1,18 @@
-import React, { useEffect, useState, useMemo, useImperativeHandle, forwardRef } from 'react';
+import React, { useMemo, useImperativeHandle, forwardRef } from 'react';
 import EmployeeItem from './employeeItem';
 import ShiftItem from './shiftItem';
 import AddShift from '../addShift';
 import EditShift from '../editShift';
-import WeekSchedule from '@/components/types/weekSchedule';
-import User from '@/components/types/user';
-import Shift from '@/components/types/shift';
 import styles from './main.module.scss';
 import { Card } from 'primereact/card';
 import { Paginator, PaginatorPageChangeEvent } from 'primereact/paginator';
 import '@/styles/components/pagination.css';
 import LoadingSpinner from '@/components/loadingSpinner';
-import { fetchFilterUsersInSchedule, fetchUserScheduleWithAbsences } from '@/services/scheduleService';
 import { useRoles } from '@/hooks/roles/useRoles';
 import { useShiftManagement } from '@/hooks/shifts/useShiftManagement';
 import { useShiftModals } from '@/hooks/shifts/useShiftModals';
+import { usePaginationAndFiltering } from '@/hooks/usePaginationAndFilteringEmpInSchedule';
+import { useSchedule } from '@/hooks/useSchedule';
 
 interface CalendarRowProps {
   currentWeek: Date[];
@@ -23,17 +21,21 @@ interface CalendarRowProps {
 
 const CalendarRow = forwardRef(({ currentWeek, searchQuery }: CalendarRowProps, ref) => {
   const { roles, loadingRoles } = useRoles();
-  const [users, setUsers] = useState<User[]>([]);
-  const [schedules, setSchedules] = useState<Record<number, WeekSchedule>>({});
 
-  const [abortController, setAbortController] = useState<AbortController | null>(null);
+  const {
+    users,
+    loadingUsers,
+    pageNumber,
+    pageSize,
+    totalPages,
+    setPageNumber,
+    setPageSize,
+  } = usePaginationAndFiltering({ searchQuery });
 
-  const [loadingUsers, setLoadingUsers] = useState<boolean>(true);
-  const [loadingSchedules, setLoadingSchedules] = useState<boolean>(true);
-
-  const [pageNumber, setPageNumber] = useState(1);
-  const [pageSize, setPageSize] = useState(20);
-  const [totalPages, setTotalPages] = useState(0);
+  const {
+    schedules,
+    loadingSchedules,
+    setSchedules } = useSchedule({ users, currentWeek });
 
   const {
     handleAddShift,
@@ -53,69 +55,11 @@ const CalendarRow = forwardRef(({ currentWeek, searchQuery }: CalendarRowProps, 
     setIsEditModalOpen,
   } = useShiftModals();
 
-  useEffect(() => {
-    const loadData = async () => {
-      setLoadingUsers(true);
-      await fetchFilterUsersInSchedule({ query: searchQuery }, setUsers, setTotalPages, pageNumber, pageSize)
-      setLoadingUsers(false);
-    }
-    loadData();
-  }, [searchQuery, pageNumber, pageSize]);
-
-  useEffect(() => {
-    if (users.length > 0) {
-      setLoadingSchedules(true);
-      if (abortController) {
-        abortController.abort();
-      }
-
-      const newAbortController = new AbortController();
-      setAbortController(newAbortController);
-
-      const fetchSchedules = async () => {
-        try {
-          const schedulePromises = users.map(user =>
-            fetchUserScheduleWithAbsences(user.id, currentWeek)
-              .then(data => {
-                return { userId: user.id, data: data };
-              })
-              .catch(error => {
-                if (error.name === 'AbortError') {
-                } else {
-                  console.error(`Error fetching schedule for user ${user.id}:`, error);
-                }
-                return { userId: user.id, data: { shifts: [], absences: [] } };
-              })
-          );
-
-          const results = await Promise.all(schedulePromises);
-          const schedulesMap: Record<number, WeekSchedule> = {};
-
-          results.forEach(({ userId, data }) => {
-            schedulesMap[userId] = {
-              shifts: data.shifts || [],
-              absences: data.absences || []
-            };
-          });
-
-          setSchedules(schedulesMap);
-        } catch (error) {
-          console.error('Error fetching schedules:', error);
-        } finally {
-          setLoadingSchedules(false);
-        }
-      };
-
-      fetchSchedules();
-    }
-  }, [users, currentWeek]);
-
   useImperativeHandle(ref, () => ({
     publishAll: handlePublishAll,
   }));
 
   const renderedRows = useMemo(() => {
-
     const getMoreInfoOfEmployee = (user_id: number) => {
       const url = `http://localhost:3000/employees/user/${user_id}`;
       window.open(url, '_blank');
---
+++ hooks/useSchedule.ts
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react';
+import WeekSchedule from '@/components/types/weekSchedule';
+import User from '@/components/types/user';
+import { fetchUserScheduleWithAbsences } from '@/services/scheduleService';
+
+interface UseScheduleManagementProps {
+    users: User[];
+    currentWeek: Date[];
+}
+
+export const useSchedule = ({ users, currentWeek }: UseScheduleManagementProps) => {
+    const [schedules, setSchedules] = useState<Record<number, WeekSchedule>>({});
+    const [loadingSchedules, setLoadingSchedules] = useState<boolean>(false);
+    const [abortController, setAbortController] = useState<AbortController | null>(null);
+
+
+    useEffect(() => {
+        if (users.length > 0) {
+            setLoadingSchedules(true);
+            if (abortController) {
+                abortController.abort();
+            }
+
+            const newAbortController = new AbortController();
+            setAbortController(newAbortController);
+
+            const fetchSchedules = async () => {
+                try {
+                    const schedulePromises = users.map(async (user) => {
+                        try {
+                            const data = await fetchUserScheduleWithAbsences(user.id, currentWeek);
+                            return { userId: user.id, data };
+                        } catch (error) {
+                            if (error instanceof Error && error.name === 'AbortError') {
+                                console.warn(`Fetch aborted for user ${user.id}`);
+                            } else {
+                                console.error(`Error fetching schedule for user ${user.id}:`, error);
+                            }
+                            return { userId: user.id, data: { shifts: [], absences: [] } };
+                        }
+                    });
+            
+                    const results = await Promise.all(schedulePromises);
+                    const schedulesMap: Record<number, WeekSchedule> = {};
+            
+                    results.forEach(({ userId, data }) => {
+                        schedulesMap[userId] = {
+                            shifts: data.shifts || [],
+                            absences: data.absences || [],
+                        };
+                    });
+            
+                    await setSchedules(schedulesMap);
+                } catch (error) {
+                    console.error('Error fetching schedules:', error);
+                } finally {
+                    setLoadingSchedules(false);
+                }
+            };
+
+            fetchSchedules();
+        }
+    }, [users, currentWeek]);
+
+    return {
+        schedules,
+        loadingSchedules,
+        setSchedules,
+    };
+};
