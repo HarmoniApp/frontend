642f311b443c49d3a06327e9eba0a5fc2c33a5ea
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2024-12-28 11:54:57 +0100

    refactor: shedule.

--- components/schedule/index.tsx
+++ components/schedule/index.tsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from 'react';
 import Employee from './employee';
 import Employer from './employer';
-import styles from './main.module.scss';
 import LoadingSpinner from '../loadingSpinner';
 
 const Schedule = () => {
@@ -22,13 +21,13 @@ const Schedule = () => {
   }, []);
 
   return (
-    <div className={styles.scheduleContainerMain}>
+    <>
       {userId !== 0 ? (
         isAdmin ? <Employer /> : <Employee userId={userId} />
       ) : (
         <LoadingSpinner wholeModal={false}/>
       )}
-    </div>
+    </>
   );
 };
 export default Schedule;
